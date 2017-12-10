509cdede4ec2dc9c36e5568f6eb48e625e03cca3
Tyler Haigh <Tyler_Haigh@PacificNational.com.au>
2017-12-10 13:30:39 +1100

    Broadcast server messages to all connected clients

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,10 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.emit('createMessage', {
-   from: 'Jordan' ,
-   text: 'Hi'
-});
+// socket.emit('createMessage', {
+//    from: 'Jordan' ,
+//    text: 'Hi'
+// });
 
 socket.on('newMessage', function (message) {
     console.log(message);
