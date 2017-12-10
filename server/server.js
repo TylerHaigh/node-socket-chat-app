ffa79acce06db753d6d8f7afb1ae9a69e87b817c
Tyler Haigh <Tyler_Haigh@PacificNational.com.au>
2017-12-10 11:29:48 +1100

    Setup express

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '/../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+// Middleware
+
+app.use(express.status(publicPath));
+
+// Routes
+
+
+// Listen
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
