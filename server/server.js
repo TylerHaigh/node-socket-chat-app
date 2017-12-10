978ab201048640b2d1b4e1cb9e41dea320d31b54
Tyler Haigh <Tyler_Haigh@PacificNational.com.au>
2017-12-10 13:26:33 +1100

    Add socket message events

--- server/server.js
+++ server/server.js
@@ -19,6 +19,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Tyler',
+        text: 'Hi',
+        createdAt: 123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log(message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
